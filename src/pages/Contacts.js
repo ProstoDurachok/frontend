66f6a3ca4044c81084b8832a1638a0dd1f4339ab
Hikinamuri <hikinamuri@mail.ru>
2026-04-10 13:03:30 +0300

    Добавление номера телефона

--- src/pages/Contacts.js
+++ src/pages/Contacts.js
@@ -42,7 +42,7 @@ const Contacts = () => {
         {
             name: "Салон на Ершова",
             address: "г. Казань, Николая Ершова, 47",
-            phones: ["+7 (951) 896-71-17"],
+            phones: ["+7 (843) 528-07-08", "+7 (951) 896-71-17"],
             hours: "Пн-Пт: 10:00 — 20:00 Сб: 10:00 — 18:00 Вс: выходной",
             image: Ershowa,
             mapUrl: "https://yandex.ru/maps/org/egooptika/73340528068/",
@@ -50,7 +50,7 @@ const Contacts = () => {
         {
             name: "Салон на Меридианной",
             address: "г. Казань, Меридианная, 4",
-            phones: ["+7 (939) 809-52-80"],
+            phones: ["+7 (843) 528-07-08", "+7 (939) 809-52-80"],
             hours: "Пн-Пт: 9:00 — 20:00 Сб: 10:00 — 19:00 Вс: выходной",
             image: Meridian,
             mapUrl: "https://yandex.ru/maps/org/egooptika/138533122876/",
